92859f95c76552c96b218a890951840bcbe12006
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-07-20 10:49:54 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,48 +4,62 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Работа_с_конкретными_строками__столбцами
+namespace Наибольший_элемент
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int lineSize = 4;
-            int columnSize = 4;
-
-            int[,] array = new int[lineSize, columnSize];
             Random random = new Random();
 
-            int sum = 0;
-            int product = 1;
-            int lineNumber = 2;
-            int columnNumber = 1;
-            int minNumber = 1;
-            int maxNumber = 4;
+            int lineSize = 10;
+            int columnSize = 10;
+            int[,] A = new int[lineSize, columnSize];
+            int maxElement = int.MinValue;
+            int minValue = 0;
+            int maxValue = 50;
 
-            for (int i = 0; i < array.GetLength(0); i++)
+            for (int i = 0; i < A.GetLength(0); i++)
             {
-                for (int j = 0; j < array.GetLength(1); j++)
+                for (int j = 0; j < A.GetLength(1); j++)
                 {
-                    array[i, j] = random.Next(minNumber, maxNumber);
-                    Console.Write(array[i, j] + " ");
+                    A[i, j] = random.Next(minValue, maxValue);
+                    Console.Write(A[i, j] + "  ");
+
+                    if (maxElement < A[i, j])
+                    {
+                        maxElement = A[i, j];
+                    }
                 }
 
                 Console.WriteLine();
             }
 
-            for (int i = 0; i < array.GetLength(1); i++)
-            {
-                sum += array[lineNumber - 1, i];
-            }
+            Console.WriteLine($"\nМаксимальное число массива: {maxElement}\n");
 
-            for (int i = 0; i < array.GetLength(0); i++)
+            for (int i = 0; i < A.GetLength(0); i++)
             {
-                product *= array[i, columnNumber - 1];
-            }
+                for (int j = 0; j < A.GetLength(1); j++)
+                {
+                    if (maxElement == A[i, j])
+                    {
+                        A[i, j] = 0;
 
-            Console.WriteLine($"{sum} - сумма чисел {lineNumber} строки.");
-            Console.WriteLine($"{product} - произведение чисел {columnNumber} столбца.");
+                        if (A[i, j] == 0)
+                        {
+                            Console.ForegroundColor = ConsoleColor.Red;
+                        }
+                    }
+                    else
+                    {
+                        Console.ForegroundColor = ConsoleColor.White;
+                    }
+
+                    Console.Write(A[i, j] + "  ");
+                }
+
+                Console.WriteLine();
+            }
         }
     }
 }

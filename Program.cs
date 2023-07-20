43e62845aba3395e70f3f3c5f074f94115b74a08
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-07-20 14:11:55 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -14,32 +14,33 @@ namespace Локальные_максимумы
             Random random = new Random();
 
             int arrayElements = 30;
-            int[] arrayNumbers = new int[] { 6, 16, 83, 82, 7, 68 };
+            int[] arrayNumbers = new int[arrayElements];
             int minValue = 0;
             int maxValue = 100;
 
             for (int i = 0; i < arrayNumbers.Length; i++)
             {
-               // arrayNumbers[i] = random.Next(minValue, maxValue);
+                arrayNumbers[i] = random.Next(minValue, maxValue);
                 Console.Write(arrayNumbers[i] + " ");
             }
 
             if (arrayNumbers[0] > arrayNumbers[1])
             {
-                Console.WriteLine(arrayNumbers[0]);
+                Console.Write($"\nЛокальный максимум:\n{arrayNumbers[0]} ");
             }
 
             for (int i = 1; i < arrayNumbers.Length - 2; i++)
             {
                 if (arrayNumbers[i] > arrayNumbers[i + 1] && arrayNumbers[i] > arrayNumbers[i - 1])
                 {
-                    Console.WriteLine(arrayNumbers[i]);
+                    Console.Write($"{arrayNumbers[i]} ");
                 }
             }
 
-            //if (arrayNumbers[arrayElements - 1])
-            //Console.WriteLine(arrayNumbers[arrayElements - 1] + " ");
-
+            if (arrayNumbers[arrayElements - 1] > arrayNumbers[arrayElements - 2])
+            {
+                Console.WriteLine($"{arrayNumbers[arrayElements - 1]}") ;
+            }
         }
     }
 }

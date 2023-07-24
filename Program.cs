a6024553745fd0c8f1dfb551bb5b83fbbdd26917
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-07-25 01:00:08 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,54 +4,31 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Подмассив_повторений_чисел
+namespace Сортировка_чисел
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int arrayLength = 30;
-            int[] numbers = new int[arrayLength];
-
-            Random random = new Random();
-
-            int minNumber = 1;
-            int maxNumber = 3;
-            int counter = 1;
-            int maxRepeats = 1;
-            int number = 0;
+            int[] numbers = { 10, 2, 14, 5, 6, 81, 9, 51, 1, 27 };
+            int tempNumber;
 
             for (int i = 0; i < numbers.Length; i++)
             {
-                numbers[i] = random.Next(minNumber, maxNumber);
-                Console.Write($"{numbers[i]} ");
-            }
-
-            for (int i = 0; i < numbers.Length - 1; i++)
-            {
-                if (numbers[i] == numbers[i + 1])
+                for (int j = i + 1; j < numbers.Length; j++)
                 {
-                    counter++;
-                }
-                else
-                {
-                    counter = 1;
+                    if (numbers[i] > numbers[j])
+                    {
+                        tempNumber = numbers[i];
+                        numbers[i] = numbers[j];
+                        numbers[j] = tempNumber;
+                    }
                 }
 
-                if (counter > maxRepeats)
-                {
-                    maxRepeats = counter;
-                    number = numbers[i];
-                }
-            }
-
-            if (number == 0)
-            {
-                Console.WriteLine($"Нет чисел повторяющихся подряд.");
+                Console.Write(numbers[i] + " ");
             }
 
             Console.WriteLine();
-            Console.WriteLine($"Число {number} повторяется наибольшее количество раз подряд: {maxRepeats}.");
         }
     }
 }

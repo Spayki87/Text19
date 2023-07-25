0880bad476ea078192bc85ddbb9c66d3076666d1
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-07-25 10:28:40 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -4,28 +4,18 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Сортировка_чисел
+namespace Задача_Split
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            int[] numbers = { 10, 2, 14, 5, 6, 81, 9, 51, 1, 27 };
-            int tempNumber;
+            string text = "В этом задании мне все понятно";
+            char[] textArray = new char[] { ' ' };
 
-            for (int i = 0; i < numbers.Length; i++)
+            foreach (string word in text.Split(textArray))
             {
-                for (int j = i + 1; j < numbers.Length; j++)
-                {
-                    if (numbers[i] > numbers[j])
-                    {
-                        tempNumber = numbers[i];
-                        numbers[i] = numbers[j];
-                        numbers[j] = tempNumber;
-                    }
-                }
-
-                Console.Write(numbers[i] + " ");
+                Console.WriteLine(word);
             }
 
             Console.WriteLine();

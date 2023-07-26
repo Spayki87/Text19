54a53615deb065daf81b7fef817dbbb907f0f666
Spayki87 <136745298+Spayki87@users.noreply.github.com>
2023-07-26 13:59:59 +0300

    Add files via upload

--- Program.cs
+++ Program.cs
@@ -1,26 +1,43 @@
 ﻿using System;
 using System.Collections.Generic;
-using System.Diagnostics;
+using System.Globalization;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace Задача_Split
+namespace Сдвиг_значений_массива
 {
     internal class Program
     {
         static void Main(string[] args)
         {
-            string text = "В этом задании мне все понятно-непонятно, но интерестно.";
-            char[] delimiters = new char[] { ',', ' ', '-', '.' };
-            string[] splitted = text.Split(delimiters);
+            int[] numbers = { 1, 2, 3, 4 };
+            int userInput;
 
-            foreach (string word in splitted)
+            for (int i = 0; i < numbers.Length; i++)
             {
-                Console.WriteLine(word);
+                Console.Write($"{numbers[i]} ");
             }
 
-            Console.ReadKey();
+            Console.WriteLine("\nУкажите значение сдвига массива влево: ");
+            userInput = Convert.ToInt32(Console.ReadLine());
+
+            userInput %= numbers.Length;
+
+            for (int j = 0; j < userInput; j++)
+            {
+                for (int i = 0; i < numbers.Length - 1; i++)
+                {
+                    int buffer = numbers[i];
+                    numbers[i] = numbers[i + 1];
+                    numbers[i + 1] = buffer;
+                }
+            }
+
+            for (int i = 0; i < numbers.Length; i++)
+            {
+                Console.Write($"{numbers[i]} ");
+            }
         }
     }
 }
